dbf3ef390abfeb96a8e5e2a89719b93ed2725e9a
[Jacinto Martinez] <[jacintomartinez0909@gmail.com]>
2021-10-27 21:45:23 +0200

    Updates

---
+++ precache-manifest.c43e35fc990ee75a93af2de4a6fcd187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70297ea00be6bc3ff0977f6e4afd59b",
+    "url": "/jacinto09/cocktails.git/index.html"
+  },
+  {
+    "revision": "7251639176eec7d4bfc6",
+    "url": "/jacinto09/cocktails.git/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "6c2844e6339924532bfa",
+    "url": "/jacinto09/cocktails.git/static/js/2.9e362b89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jacinto09/cocktails.git/static/js/2.9e362b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7251639176eec7d4bfc6",
+    "url": "/jacinto09/cocktails.git/static/js/main.bc9f3163.chunk.js"
+  },
+  {
+    "revision": "3a1ab8ba034555268336",
+    "url": "/jacinto09/cocktails.git/static/js/runtime-main.3f22285f.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/jacinto09/cocktails.git/static/media/logo.9a3d2645.svg"
+  }
+]);
