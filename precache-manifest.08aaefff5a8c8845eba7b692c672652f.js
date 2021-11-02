5c8f2f00229b321ec90718990d2a37c68ad258ba
[Jacinto Martinez] <[jacintomartinez0909@gmail.com]>
2021-11-02 18:31:38 +0100

    Updates

---
+++ precache-manifest.08aaefff5a8c8845eba7b692c672652f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eec28a8bbeeb5d645b17cb161ead235",
+    "url": "/jacinto09/Cocktails/index.html"
+  },
+  {
+    "revision": "9da02f4b3e28938262eb",
+    "url": "/jacinto09/Cocktails/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "6c2844e6339924532bfa",
+    "url": "/jacinto09/Cocktails/static/js/2.9e362b89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jacinto09/Cocktails/static/js/2.9e362b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9da02f4b3e28938262eb",
+    "url": "/jacinto09/Cocktails/static/js/main.809ba2b3.chunk.js"
+  },
+  {
+    "revision": "f6d7a8dfcb89ea8120e7",
+    "url": "/jacinto09/Cocktails/static/js/runtime-main.b229a4a7.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/jacinto09/Cocktails/static/media/logo.9a3d2645.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jacinto09/Cocktails.git/precache-manifest.2d098eded9182070527891455307f79f.js"
+  "/jacinto09/Cocktails/precache-manifest.08aaefff5a8c8845eba7b692c672652f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jacinto09/Cocktails.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jacinto09/Cocktails/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
